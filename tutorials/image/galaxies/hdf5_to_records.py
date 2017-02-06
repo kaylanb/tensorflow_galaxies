9fc8ea451add7407603c6d430eb655215f8d0ae3
Kaylan Burleigh <kaylanb@berkeley.edu>
2017-02-06 11:50:49 -0800

    hdf5 successfully converted to train,test,val TFRecord format

--- tutorials/image/galaxies/hdf5_to_records.py
+++ tutorials/image/galaxies/hdf5_to_records.py
@@ -18,111 +18,125 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import argparse
 import os
 import sys
+import h5py
+import numpy as np
 
 import tensorflow as tf
 
-from tensorflow.contrib.learn.python.learn.datasets import mnist
-
 # Flags for defining the tf.train.Server
-#tf.app.flags.DEFINE_string("directory", "hello", "One of 'ps', 'worker'")
-#tf.app.flags.DEFINE_integer("validation_size", 100, "Index of task within the job")
-#FLAGS = tf.app.flags.FLAGS
-
+tf.app.flags.DEFINE_string("data_dir", "/Users/kaylan1/tensorflow/galaxies/data_dir", "One of 'ps', 'worker'")
+tf.app.flags.DEFINE_integer("val_size", 200, "Index of task within the job")
+tf.app.flags.DEFINE_integer("test_size", 200, "Index of task within the job")
+tf.app.flags.DEFINE_integer("train_size", 2620, "Index of task within the job")
+tf.app.flags.DEFINE_boolean("use_ivar", False, "Index of task within the job")
+FLAGS = tf.app.flags.FLAGS
 
 def _int64_feature(value):
   return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
-#def _float_feature(value):
-#  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
-
 
 def _bytes_feature(value):
   return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
-def convert_to(data_set, name):
-  """Converts a dataset to tfrecords."""
-  # Nsample x 200 x 200 x 3 numpy array of type float32
-  images = data_set.images
-  # Nsample numpy array of type uint8
-  labels = data_set.labels
-  num_examples = data_set.num_examples
+def convert_to(data_set, savename='test'):
+	"""Converts a dataset to tfrecords."""
+	filename = os.path.join(FLAGS.data_dir, savename + '.tfrecords')
+	if not os.path.exists(filename):
+		# Nsample x 200 x 200 x 3 numpy array of type float32
+		images = data_set.images
+		# Nsample numpy array of type uint8
+		labels = data_set.labels
+		num_examples = data_set.num_examples
 
-  if images.shape[0] != num_examples:
-    raise ValueError('Images size %d does not match label size %d.' %
-                     (images.shape[0], num_examples))
-  rows = images.shape[1]
-  cols = images.shape[2]
-  depth = images.shape[3]
+		if images.shape[0] != num_examples:
+			raise ValueError('Images size %d does not match label size %d.' %
+											 (images.shape[0], num_examples))
+		rows = images.shape[1]
+		cols = images.shape[2]
+		depth = images.shape[3]
 
-  filename = os.path.join(FLAGS.directory, name + '.tfrecords')
-  print('Writing', filename)
-  writer = tf.python_io.TFRecordWriter(filename)
-  for index in range(num_examples):
-    image_raw = images[index].tostring()
-    example = tf.train.Example(features=tf.train.Features(feature={
-        'height': _int64_feature(rows),
-        'width': _int64_feature(cols),
-        'depth': _int64_feature(depth),
-        'label': _int64_feature(int(labels[index])),
-        'image_raw': _bytes_feature(image_raw)}))
-    writer.write(example.SerializeToString())
-  writer.close()
+		writer = tf.python_io.TFRecordWriter(filename)
+		for index in range(num_examples):
+			image_raw = images[index].tostring()
+			example = tf.train.Example(features=tf.train.Features(feature={
+					'height': _int64_feature(rows),
+					'width': _int64_feature(cols),
+					'depth': _int64_feature(depth),
+					'label': _int64_feature(int(labels[index])),
+					'image_raw': _bytes_feature(image_raw)}))
+			writer.write(example.SerializeToString())
+		writer.close()
+		print('Wrote', filename)
+	else:
+		print('Already exists',filename)
 
 
 def add_invvar(stamp_ivar,img_ivar):
-    '''stamp_ivar, img_ivar -- galsim Image objects'''
-    # Use img_ivar when stamp_ivar == 0, both otherwise
-    use_img_ivar= np.ones(img_ivar.shape).astype(bool)
-    use_img_ivar[ stamp_ivar > 0 ] = False
-    # Compute using both
-    ivar= np.power(stamp_ivar.copy(), -1) + np.power(img_ivar.copy(), -1)
-    ivar= np.power(ivar,-1)
-    keep= np.ones(ivar.shape).astype(bool)
-    keep[ (stamp_ivar > 0)*\
-          (img_ivar > 0) ] = False
-    ivar[keep] = 0.
-    # Now use img_ivar only where need to
-    ivar[ use_img_ivar ] = img_ivar.copy()[ use_img_ivar ]
-    return ivar
+	'''stamp_ivar, img_ivar -- galsim Image objects'''
+	# Use img_ivar when stamp_ivar == 0, both otherwise
+	use_img_ivar= np.ones(img_ivar.shape).astype(bool)
+	use_img_ivar[ stamp_ivar > 0 ] = False
+	# Compute using both
+	ivar= np.power(stamp_ivar.copy(), -1) + np.power(img_ivar.copy(), -1)
+	ivar= np.power(ivar,-1)
+	keep= np.ones(ivar.shape).astype(bool)
+	keep[ (stamp_ivar > 0)*\
+				(img_ivar > 0) ] = False
+	ivar[keep] = 0.
+	# Now use img_ivar only where need to
+	ivar[ use_img_ivar ] = img_ivar.copy()[ use_img_ivar ]
+	return ivar
 
 class BuildTraining(object):
-    """
-    Takes hdf5 file created by GatherTraining and saves as single N-Dim array
-    that will be converted to tensorflow format 
-    """ 
-        
-    def __init__(self,data_dir=None):
+	"""
+	Takes hdf5 file created by GatherTraining and saves as single N-Dim array
+	that will be converted to tensorflow format 
+	""" 
+	def __init__(self,data_dir=None):
 		assert(not data_dir is None)
 		self.data_dir= data_dir
-        self.f= h5py.File(os.path.join(self.data_dir,'training_10k.hdf5'),'r')
+		self.f= h5py.File(os.path.join(self.data_dir,'training_3k.hdf5'),'r')
+		# map classification to integer
+		labs= ['elg','lrg','qso','star','back']
+		self.cats={}
+		for lab,val in zip(labs,range(len(labs))):
+			self.cats[lab]= val
 
-    def create_srcpimage(self,use_ivar=False):
+	def create_srcpimage(self,use_ivar=False):
 		if use_ivar:
-			savefn= os.path.join(self.data_dir,'training_10k_ivar.hdf5')
+			savefn= os.path.join(self.data_dir,'training_3k_ivar.hdf5')
 		else:	
-			savefn= os.path.join(self.data_dir,'training_10k_noivar.hdf5')
+			savefn= os.path.join(self.data_dir,'training_3k_noivar.hdf5')
 		fobj= h5py.File(savefn,'a')
 		node= '/srcpimg'
+		n_samples= len(self.f.keys()) * 5
 		if not node in fobj:
 			if use_ivar:	
-				data= np.zeros(10000,200,200,3).astype(np.float32)+np.nan
+				data= np.zeros((n_samples,200,200,6)).astype(np.float32)+np.nan
 			else:
-				data= np.zeros(10000,200,200,6).astype(np.float32)+np.nan
-			label= np.zeros(10000).astype(str)
-			for cnt,id in enumerate(self.f.keys()):
-				for obj in ['elg','lrg']:
-					print(self.f['/%s/%s' % (id,obj)].shape)
-					img= src+back
-					if use_invvar:
+				data= np.zeros((n_samples,200,200,3)).astype(np.float32)+np.nan
+			#label= np.zeros(n_samples).astype(np.string_) # Fixed ASCII for hdf5 
+			label= np.zeros(n_samples).astype(np.int8)-1 
+			cnt=-1
+			for id in list(self.f.keys()):
+				for obj in ['elg','lrg','qso','star','back']:
+					cnt += 1
+					if cnt % 100 == 0:
+						print('Storing image %d/%d' % (cnt,n_samples))
+					label[cnt]= self.classToInt(obj=obj)
+					if obj == 'back':
+						data[cnt,...]= np.array(self.f['/%s/%s' % (id,obj)])
+					else:
+						data[cnt,...]= np.array(self.f['/%s/%s' % (id,obj)]) + np.array(self.f['/%s/%s' % (id,'back')])
+					if use_ivar:
 						ivar= add_invvar(src_ivar,back_ivar)
-				if cnt > 5: break
 			assert( np.where( np.isfinite(data).flatten() == False )[0].size == 0)
+			assert( np.where( label < 0 )[0].size == 0)
 			dset = fobj.create_dataset('%s/images' % node, data=data,chunks=True)
 			dset = fobj.create_dataset('%s/labels' % node, data=label,chunks=True)
+			#dset = fobj.create_dataset('%s/labels' % node, data=label,dtype="S4",chunks=True)
 		print('srcpimage_noivar has been created')
 		return fobj
 
@@ -130,78 +144,40 @@ class BuildTraining(object):
 		fobj= self.create_srcpimage(use_ivar=use_ivar)
 		return fobj['/srcpimg/images'],fobj['/srcpimg/labels']
 
-def main(unused_argv):
-  # Get the data.
-  #data_sets = mnist.read_data_sets(FLAGS.directory,
-  #                                 dtype=tf.uint8,
-  #                                 reshape=False,
-  #                                 validation_size=FLAGS.validation_size)
-  builder= BuildTraining(data_dir=FLAGS.directory)
-  images,labels= builder.get_srcpimage(use_ivar=FLAGS.use_ivar)
-  raise ValueError
-  # Python3 h5py containers arg!
-  #keys= list(fobj.keys())
-  #keys2= list(fobj[keys[0]].keys())
-  #fobj['/95941990/elg'].shape
-  #fobj['/*/elg'].shape
+	def classToInt(self,obj=None):
+		assert(obj in self.cats.keys())
+		return self.cats[obj]
 
-  # Shuffle samples
-  inds= np.shuffle(range(dset.shape[0]))
-  keep={}
-  keep['test']= inds[ :FLAGS.test_size ]
-  keep['val']= inds[ FLAGS.test_size:FLAGS.val_size ]
-  keep['train']= inds[ FLAGS.test_size+FLAGS.val_size: ]
-  for key,sz in zip(['test','val','train'],[1000,1000,8000]):
-	assert(keep[key].size == sz)
-  # data set object
-  class DataSet(object):
-    def __init__(self,images,labels,keep):
-		self.images= images[keep]
-		self.labels= labels[keep]
-		self.data_set.num_examples = keep.size
-  data={}
-  for key in keep.keys():
-    data[key]= DataSet(images,labels,keep[key])
-  # Convert to Examples and write the result to TFRecords.
-  convert_to(data['train'], 'train')
-  convert_to(data['val'], 'validation')
-  convert_to(data['test'], 'test')
+
+def main(unused_argv):
+	builder= BuildTraining(data_dir=FLAGS.data_dir)
+	images,labels= builder.get_srcpimage(use_ivar=FLAGS.use_ivar)
+
+	# Shuffle samples
+	inds= np.arange(images.shape[0])
+	np.random.shuffle(inds)
+	keep={}
+	keep['test']= inds[ :FLAGS.test_size ]
+	keep['val']= inds[ FLAGS.test_size: FLAGS.test_size+FLAGS.val_size ]
+	keep['train']= inds[ FLAGS.test_size+FLAGS.val_size: ]
+	for key,sz in zip(['test','val','train'],[FLAGS.test_size,FLAGS.val_size,FLAGS.train_size]):
+		assert(keep[key].size == sz)
+	# Convert to TFRecords
+	class DataSet(object):
+		def __init__(self,images,labels,keep):
+			self.images= np.array(images)[keep]
+			self.labels= np.array(labels)[keep]
+			self.num_examples = keep.size
+	data={}
+	for key in keep.keys():
+		data[key]= DataSet(images,labels,keep[key])
+		# Write as TFRecords.
+		savename='%s_3k_noivar' % key
+		convert_to(data[key], savename='%s_3k_noivar' % key)
+	print('Done')
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--directory',
-      type=str,
-      default='/Users/kaylan1/tensorflow/galaxies/data_dir',
-      help='Directory to download data files and write the converted result'
-  )
-  parser.add_argument(
-      '--val_size',
-      type=int,
-      default=1000,
-      help="""\
-      Number of examples to separate from the training data for the validation
-      set.\
-      """
-  )
-  parser.add_argument(
-      '--test_size',
-      type=int,
-      default=1000,
-      help="""\
-      Number of examples to separate from the training data for the validation
-      set.\
-      """
-  )
-  parser.add_argument(
-      '--use_ivar',
-      action='store_true',
-      help="""\
-      put invvar in the samples\
-      """
-  )
-  FLAGS, unparsed = parser.parse_known_args()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+  tf.app.run(main=main, argv=[sys.argv[0]])
 
 
