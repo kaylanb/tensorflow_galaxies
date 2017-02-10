44b1764a1dfe4bb5d3acce02a110791a67684598
Kaylan Burleigh <kaylanb@berkeley.edu>
2017-02-10 11:19:40 -0800

    simple.py ready for testing just need properlly load in training data"

--- tutorials/image/galaxies/simple.py
+++ tutorials/image/galaxies/simple.py
@@ -85,6 +85,52 @@ class HyperParams(TrainingProps):
 		return dict(first=first,\
 								second=second)
 
+
+def _variable_on_cpu(name, shape, initializer):
+  """Helper to create a Variable stored on CPU memory.
+
+  Args:
+    name: name of the variable
+    shape: list of ints
+    initializer: initializer for Variable
+
+  Returns:
+    Variable Tensor
+  """
+  with tf.device('/cpu:0'):
+    dtype = tf.float32
+    var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
+  return var
+
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+  """Helper to create an initialized Variable with weight decay.
+
+  Note that the Variable is initialized with a truncated normal distribution.
+  A weight decay is added only if one is specified.
+
+  Args:
+    name: name of the variable
+    shape: list of ints
+    stddev: standard deviation of a truncated Gaussian
+    wd: add L2Loss weight decay multiplied by this float. If None, weight
+        decay is not added for this Variable.
+
+  Returns:
+    Variable Tensor
+  """
+  dtype = tf.float32
+  var = _variable_on_cpu(
+      name,
+      shape,
+      tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
+  if wd is not None:
+    weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+    tf.add_to_collection('losses', weight_decay)
+  return var
+
+
+
 def inference(images, hyper=None):
   """Build the CIFAR-10 model.
 
@@ -191,38 +237,82 @@ def loss(logits, labels):
   """
   labels = tf.to_int64(labels)
   cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
-      labels=labels, logits=logits, name='xentropy')
-  return tf.reduce_mean(cross_entropy, name='xentropy_mean')
+      labels=labels, logits=logits, name='cross_entropy_per_example')
+  cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+  tf.add_to_collection('losses', cross_entropy_mean)
+  # The total loss is defined as the cross entropy loss plus all of the weight
+  # decay terms (L2 loss).
+  return tf.add_n(tf.get_collection('losses'), name='total_loss')
+  # MNIST:
+  #cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+  #    labels=labels, logits=logits, name='xentropy')
+  #return tf.reduce_mean(cross_entropy, name='xentropy_mean')
 
+def train(total_loss, global_step):
+  """Train CIFAR-10 model.
 
-
-def training(loss, learning_rate):
-  """Sets up the training Ops.
-
-  Creates a summarizer to track the loss over time in TensorBoard.
-
-  Creates an optimizer and applies the gradients to all trainable variables.
-
-  The Op returned by this function is what must be passed to the
-  `sess.run()` call to cause the model to train.
+  Create an optimizer and apply to all trainable variables. Add moving
+  average for all trainable variables.
 
   Args:
-    loss: Loss tensor, from loss().
-    learning_rate: The learning rate to use for gradient descent.
-
+    total_loss: Total loss from loss().
+    global_step: Integer Variable counting the number of training steps
+      processed.
   Returns:
-    train_op: The Op for training.
+    train_op: op for training.
   """
-  # Add a scalar summary for the snapshot loss.
-  tf.summary.scalar('loss', loss)
-  # Create the gradient descent optimizer with the given learning rate.
-  optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-  # Create a variable to track the global step.
-  global_step = tf.Variable(0, name='global_step', trainable=False)
-  # Use the optimizer to apply the gradients that minimize the loss
-  # (and also increment the global step counter) as a single training step.
-  train_op = optimizer.minimize(loss, global_step=global_step)
+  # Variables that affect learning rate.
+  num_batches_per_epoch = FLAGS.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / FLAGS.batch_size
+  decay_steps = int(num_batches_per_epoch * FLAGS.NUM_EPOCHS_PER_DECAY)
+
+  # Decay the learning rate exponentially based on the number of steps.
+  lr = tf.train.exponential_decay(FLAGS.INITIAL_LEARNING_RATE,
+                                  global_step,
+                                  decay_steps,
+                                  FLAGS.LEARNING_RATE_DECAY_FACTOR,
+                                  staircase=True)
+  tf.summary.scalar('learning_rate', lr)
+
+  # Generate moving averages of all losses and associated summaries.
+  loss_averages_op = _add_loss_summaries(total_loss)
+
+  # Compute gradients.
+  with tf.control_dependencies([loss_averages_op]):
+    opt = tf.train.GradientDescentOptimizer(lr)
+    grads = opt.compute_gradients(total_loss)
+
+  # Apply gradients.
+  apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+  # Add histograms for trainable variables.
+  for var in tf.trainable_variables():
+    tf.summary.histogram(var.op.name, var)
+
+  # Add histograms for gradients.
+  for grad, var in grads:
+    if grad is not None:
+      tf.summary.histogram(var.op.name + '/gradients', grad)
+
+  # Track the moving averages of all trainable variables.
+  variable_averages = tf.train.ExponentialMovingAverage(
+      FLAGS.MOVING_AVERAGE_DECAY, global_step)
+  variables_averages_op = variable_averages.apply(tf.trainable_variables())
+
+  with tf.control_dependencies([apply_gradient_op, variables_averages_op]):
+    train_op = tf.no_op(name='train')
+
   return train_op
+  # MNIST simpler verions
+  ## Add a scalar summary for the snapshot loss.
+  #tf.summary.scalar('loss', loss)
+  ## Create the gradient descent optimizer with the given learning rate.
+  #optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+  ## Create a variable to track the global step.
+  #global_step = tf.Variable(0, name='global_step', trainable=False)
+  ## Use the optimizer to apply the gradients that minimize the loss
+  ## (and also increment the global step counter) as a single training step.
+  #train_op = optimizer.minimize(loss, global_step=global_step)
+  #return train_op
 
 
 def evaluation(logits, labels):
@@ -237,12 +327,15 @@ def evaluation(logits, labels):
     A scalar int32 tensor with the number of examples (out of batch_size)
     that were predicted correctly.
   """
+  # Simplified version of MNIST
   # For a classifier model, we can use the in_top_k Op.
   # It returns a bool tensor with shape [batch_size] that is true for
   # the examples where the label is in the top k (here k=1)
   # of all logits for that example.
-  correct = tf.nn.in_top_k(logits, labels, 1)
+  #correct = tf.nn.in_top_k(logits, labels, 1)
+  correct = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels)
   # Return the number of true entries.
+  # I think this is an average over the batches?
   return tf.reduce_sum(tf.cast(correct, tf.int32))
 
 
@@ -293,15 +386,13 @@ def main():
                              #100, 10)
 
     # Add to the Graph the Ops for loss calculation.
-    loss = mnist.loss(logits, y)
+    loss = loss(logits, y)
 
     # Add to the Graph the Ops that calculate and apply gradients.
-    train_op = mnist.training(loss, FLAGS.learning_rate)
+    train_op = training(loss, global_step)
     
-train_op = cifar10.train(loss, global_step)
-
     # Add the Op to compare the logits to the labels during evaluation.
-    eval_correct = mnist.evaluation(logits, labels_placeholder) #using sparse_softmax_cross_entropy_with_logits
+    eval_correct = evaluation(logits, labels_placeholder) #using sparse_softmax_cross_entropy_with_logits
 
     # Add the variable initializer Op.
     init = tf.global_variables_initializer()
@@ -327,8 +418,8 @@ train_op = cifar10.train(loss, global_step)
           # inspect the values of your Ops or variables, you may include them
           # in the list passed to sess.run() and the value tensors will be
           # returned in the tuple from the call.
-          _, loss_value, xnp_ = sess.run([train_op, loss, x_],
-                                   feed_dict=feed_dict)
+          _, loss_value, eval_value, xnp_ = sess.run([train_op, loss, eval_correct,x_],
+																										 feed_dict=feed_dict)
 
       duration = time.time() - start_time
 
